4eae252c35a710a796c99915c3d25029a0b820ad
Justin Privitera <privitera1@llnl.gov>
2026-01-02 16:37:17 -0800

    remove faulty added logic

--- src/libs/conduit/conduit_generator.cpp
+++ src/libs/conduit/conduit_generator.cpp
@@ -2605,18 +2605,8 @@ Generator::Parser::YAML::walk_yaml_schema(Node *node,
                                           index_t curr_offset,
                                           const bool external)
 {
-    bool empty_case = false;
-    if (check_yaml_is_scalar_node(yaml_node))
-    {
-        const std::string yaml_str = get_yaml_string(yaml_node);
-        if ("" == yaml_str)
-        {
-            empty_case = true;
-        }
-    }
-
     // object cases
-    if (check_yaml_is_mapping_node(yaml_node) || empty_case)
+    if (check_yaml_is_mapping_node(yaml_node))
     {
         // if dtype is an object, we have a "list_of" case
         const yaml_node_t *dt_value = fetch_yaml_node_from_object_by_name(yaml_doc, yaml_node, "dtype");
