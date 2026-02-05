459103a6dc1eb4959bde434214d6025cf72af8de
Cyrus Harrison <cyrush@llnl.gov>
2026-02-05 09:59:57 -0800

    missed common headers in a few modules

--- src/libs/blueprint/python/conduit_blueprint_mcarray_examples_python.cpp
+++ src/libs/blueprint/python/conduit_blueprint_mcarray_examples_python.cpp
@@ -6,20 +6,7 @@
 //-----------------------------------------------------------------------------
 // -- Python includes (these must be included first) -- 
 //-----------------------------------------------------------------------------
-#include <Python.h>
-#include <structmember.h>
-#include "bytesobject.h"
-
-#if PY_MAJOR_VERSION >= 3
-#define IS_PY3K
-#endif
-
-// use  proper strdup
-#ifdef CONDUIT_PLATFORM_WINDOWS
-    #define _conduit_strdup _strdup
-#else
-    #define _conduit_strdup strdup
-#endif
+#include "conduit_python_common.h"
 
 //-----------------------------------------------------------------------------
 // -- standard lib includes -- 
