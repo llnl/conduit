f1fcd0daed75a95e10e1c8f6ddeb35500d32bad2
Justin Privitera <privitera1@llnl.gov>
2026-03-24 11:06:12 -0700

    changelog

--- src/libs/blueprint/conduit_blueprint_mesh_matset_xforms.cpp
+++ src/libs/blueprint/conduit_blueprint_mesh_matset_xforms.cpp
@@ -1996,7 +1996,7 @@ count_elements_from_matset(const conduit::Node &matset)
     // extra seat belt here
     if (! matset.dtype().is_object())
     {
-        CONDUIT_ERROR("blueprint::mesh::matset::count_zones_in_matset"
+        CONDUIT_ERROR("blueprint::mesh::matset::count_elements_from_matset"
                       " passed matset node must be a valid matset tree.");
     }
 
@@ -2049,7 +2049,7 @@ count_elements_from_matset(const conduit::Node &matset)
         // material-dominant uni-buffer
         else
         {
-            CONDUIT_ERROR("blueprint::mesh::matset::count_zones_in_matset() "
+            CONDUIT_ERROR("blueprint::mesh::matset::count_elements_from_matset() "
                           "material-dominant uni-buffer material set is unsupported.");
         }
     }
