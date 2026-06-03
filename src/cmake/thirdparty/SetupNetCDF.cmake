4ca76481b9af626e6e221c0c59c91a3723b0b45c
Cyrus Harrison <cyrush@llnl.gov>
2026-06-03 12:06:59 -0700

    cleanup

--- src/cmake/thirdparty/SetupNetCDF.cmake
+++ src/cmake/thirdparty/SetupNetCDF.cmake
@@ -11,18 +11,6 @@ endif()
 
 message(STATUS "Looking for NetCDF in: ${NETCDF_DIR}")
 
-# set(_RAJA_SEARCH_PATH)
-# if(EXISTS ${RAJA_DIR}/share/raja/cmake)
-#   # old install layout
-#   set(_RAJA_SEARCH_PATH ${RAJA_DIR}/share/raja/cmake)
-# elseif(EXISTS ${RAJA_DIR}/lib/cmake/raja)
-#   # new install layout
-#   set(_RAJA_SEARCH_PATH ${RAJA_DIR}/lib/cmake/raja)
-# else ()
-#   # try RAJA_DIR itself
-#   set(_RAJA_SEARCH_PATH ${RAJA_DIR})
-# endif()
-
 find_package(netCDF REQUIRED
              NO_DEFAULT_PATH
              PATHS ${NETCDF_DIR})
@@ -30,8 +18,6 @@ message(STATUS "Found NetCDF in: ${NETCDF_DIR}")
 
 set(NETCDF_FOUND TRUE)
 
-# reset RAJA_DIR just in case the find process mangled it
-# set(RAJA_DIR ${RAJA_DIR_ORIG})
 if(CONDUIT_ENABLE_TESTS AND WIN32 AND BUILD_SHARED_LIBS)
     # if we are running tests with dlls, we need path to dlls
     list(APPEND CONDUIT_TPL_DLL_PATHS ${NETCDF_DIR}/lib/)
