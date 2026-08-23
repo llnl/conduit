73c0e9483a5d6c2d56dd1a20a7d0def22f031d7c
Cyrus Harrison <cyrush@llnl.gov>
2026-08-23 12:15:21 -0700

    remove uness extra code

--- src/tests/relay/t_relay_smoke.cpp
+++ src/tests/relay/t_relay_smoke.cpp
@@ -9,7 +9,6 @@
 //-----------------------------------------------------------------------------
 
 #include "conduit_relay.hpp"
-#include "conduit_relay_io.hpp"
 #include <iostream>
 #include "gtest/gtest.h"
 
@@ -18,24 +17,4 @@ using namespace conduit;
 TEST(conduit_relay_smoke, about)
 {
     std::cout << relay::about() << std::endl;
-    Node n;
-    
-    std::cout << n.to_json() << std::endl;
-    
-    std::cout << n.dtype().to_string() << std::endl;
-    std::cout << n.number_of_children() << std::endl;
-    n.set(DataType::object());
-
-    std::cout << n.dtype().to_string() << std::endl;
-    std::cout << n.number_of_children() << std::endl;
-    
-    
-    n["here"].set(DataType::empty());
-    conduit::relay::io::save(n,"here.hdf5");
-    
-    Node n2;
-
-    conduit::relay::io::load("here.hdf5",n2);
-    std::cout << n2.to_json() << std::endl;
-
 }
